9422d40793f0fd5cc314b96a9f13e18bd2c6279f
nguyenhoanganhtuan1206 <nguyenhoanganhtuan1206@gmail.com>
2024-04-09 09:17:22 +0700

    New commit for new version

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,13 +1,13 @@
 import {
-    Body,
-    Controller,
-    Get,
-    HttpCode,
-    HttpStatus,
-    Post,
-    Put,
-    UseGuards,
-    Version,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Put,
+  UseGuards,
+  Version,
 } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
@@ -26,95 +26,102 @@ import { AuthService } from '../services/auth.service';
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-    constructor(
-        private userService: UserService,
-        private authService: AuthService,
-    ) { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) { }
 
-    @Post('admin/login')
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({
-        type: LoginPayloadDto,
-        description: 'Admin info with access token',
-    })
-    async adminLogin(
-        @Body() userLoginDto: UserLoginDto,
-    ): Promise<LoginPayloadDto> {
-        const userEntity = await this.authService.validateAdmin(userLoginDto);
-        const userRoles = userEntity.permissions.map(
-            (permission) => permission.role,
-        );
-        const token = await this.authService.createAccessToken({
-            roles: userRoles,
-            userId: userEntity.id,
-        });
+  @Post('admin/login')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    type: LoginPayloadDto,
+    description: 'Admin info with access token',
+  })
+  async adminLogin(
+    @Body() userLoginDto: UserLoginDto,
+  ): Promise<LoginPayloadDto> {
+    const userEntity = await this.authService.validateAdmin(userLoginDto);
+    const userRoles = userEntity.permissions.map(
+      (permission) => permission.role,
+    );
+    const token = await this.authService.createAccessToken({
+      roles: userRoles,
+      userId: userEntity.id,
+    });
 
-        const userDto = userEntity.toDto();
+    const userDto = userEntity.toDto();
 
-        return new LoginPayloadDto(userDto, token);
-    }
+    return new LoginPayloadDto(userDto, token);
+  }
 
-    @Get('test')
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({})
-    testing(): string {
-        return 'Testing';
-    }
+  @Get('test')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({})
+  testing(): string {
+    return 'Testing';
+  }
 
-    @Post('login')
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({
-        type: LoginPayloadDto,
-        description: 'User info with access token',
-    })
-    async userLogin(
-        @Body() userLoginDto: UserLoginDto,
-    ): Promise<LoginPayloadDto> {
-        const userEntity = await this.authService.validateUser(userLoginDto);
-        const userRoles = userEntity.permissions.map(
-            (permission) => permission.role,
-        );
-        const token = await this.authService.createAccessToken({
-            roles: userRoles,
-            userId: userEntity.id,
-        });
+  @Get('test_1')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({})
+  testing_1(): string {
+    return 'Testing_1';
+  }
 
-        const userDto = userEntity.toDto();
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    type: LoginPayloadDto,
+    description: 'User info with access token',
+  })
+  async userLogin(
+    @Body() userLoginDto: UserLoginDto,
+  ): Promise<LoginPayloadDto> {
+    const userEntity = await this.authService.validateUser(userLoginDto);
+    const userRoles = userEntity.permissions.map(
+      (permission) => permission.role,
+    );
+    const token = await this.authService.createAccessToken({
+      roles: userRoles,
+      userId: userEntity.id,
+    });
 
-        return new LoginPayloadDto(userDto, token);
-    }
+    const userDto = userEntity.toDto();
 
-    @Post('generate-access-token')
-    @Version('1/external')
-    @Auth([RoleType.ADMIN])
-    @UseGuards(JwtAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({
-        description: 'Generate access token for external user',
-        type: TokenPayloadDto,
-    })
-    generateAccessTokenForExternalUser(): Promise<TokenPayloadDto> {
-        return this.authService.createExternalUserAccessToken({
-            userId: 0,
-        });
-    }
+    return new LoginPayloadDto(userDto, token);
+  }
 
-    @Version('1')
-    @Get('me')
-    @HttpCode(HttpStatus.OK)
-    @Auth([RoleType.USER, RoleType.ADMIN, RoleType.ASSISTANT])
-    @UseGuards(JwtAuthGuard)
-    @ApiOkResponse({ type: UserDto, description: 'current user info' })
-    getCurrentUser(@AuthUser() user: UserEntity): UserDto {
-        return user.toDto();
-    }
+  @Post('generate-access-token')
+  @Version('1/external')
+  @Auth([RoleType.ADMIN])
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Generate access token for external user',
+    type: TokenPayloadDto,
+  })
+  generateAccessTokenForExternalUser(): Promise<TokenPayloadDto> {
+    return this.authService.createExternalUserAccessToken({
+      userId: 0,
+    });
+  }
 
-    @Put('forgot-password')
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({
-        description: 'Request forgot password',
-    })
-    forgotPassword(@Body() forgotPassword: ForgotPassswordDto): Promise<void> {
-        return this.userService.forgotPassword(forgotPassword.email);
-    }
+  @Version('1')
+  @Get('me')
+  @HttpCode(HttpStatus.OK)
+  @Auth([RoleType.USER, RoleType.ADMIN, RoleType.ASSISTANT])
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: UserDto, description: 'current user info' })
+  getCurrentUser(@AuthUser() user: UserEntity): UserDto {
+    return user.toDto();
+  }
+
+  @Put('forgot-password')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Request forgot password',
+  })
+  forgotPassword(@Body() forgotPassword: ForgotPassswordDto): Promise<void> {
+    return this.userService.forgotPassword(forgotPassword.email);
+  }
 }
